1e0eb3cc2e0d5d0d2d7206418832adc9bae40462
User <burberrry3@gmail.com>
2023-04-04 18:08:58 +0300

    Added dark theme

--- app/src/main/java/com/example/dot/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dot/ui/theme/Theme.kt
@@ -33,9 +33,9 @@ private val LightColorPalette = lightColors(
 @Composable
 fun DotTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        LightColorPalette
-    } else {
         DarkColorPalette
+    } else {
+        LightColorPalette
     }
 
     MaterialTheme(
